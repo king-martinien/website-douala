22ac2baf76118562bb239e3ca3378ffc0ab71b32
king-martinien <martinienfokoue@yahoo.com>
2022-05-15 20:21:23 +0100

    Ajout des photos réeles sur les pages Accueil, hotels, restaurants et lieux touristiques

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+let hamburgerBtn = document.querySelector(".hamburger-btn");
+let navbar = document.querySelector('.navbar-menu');
+
+hamburgerBtn.addEventListener("click", () => {
+  hamburgerBtn.classList.toggle("hamburger-active");
+  navbar.classList.toggle('active');
+});
